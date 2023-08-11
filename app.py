e2820e364e0ec1d0f7146028b27b70029b0a047f
Chris Mannina <machris@umich.edu>
2023-08-11 13:55:58 -0400

    gitignore and about section updates

--- app.py
+++ app.py
@@ -90,12 +90,12 @@ def main():
             st.markdown("### Medical Document Q&A")
             st.markdown("#### How it works?")
             st.markdown(
-                """When a document is uploaded, text is extracted from the document. This text is then split into shorter text chunks, 
-                        and an embedding is created for each text chunk. When the user asks a question, an embedding is created for the question, 
-                        and a similarity search is performed to find the file chunk embeddings that are most similar to the question (i.e. have highest 
-                        cosine similarities with the question embedding). An API call is then made to the completions endpoint, with the question and 
-                        the most relevant file chunks are included in the prompt. The generative model then gives the answer to the question found in 
-                        the file chunks, if the answer can be found in the extracts.
+                """When a document is uploaded, text is extracted from the document. This text is then split into smaller text chunks, 
+                        and an embedding is created for each chunk. When the user asks a question, an embedding is created for the question, 
+                        and a similarity search is performed to find the document chunks that are most similar to the question. Once the
+                        relevant documents are return, a prompt is crafted with the user question and relevant context, and an API call is 
+                        then made to the chat completions endpoint. The LLM model then gives the answer to the question using the document
+                        chunks as context. 
                         """
             )
             st.markdown("---")
